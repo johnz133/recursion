e73767d4197822bab6cf1c013d840b006541f1bc
John Zhang <john@zhang.io>
2015-01-27 23:07:40 -0800

    finished getclassname

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,7 +5,16 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
+	return getElementsByClassNameHelper(className, Array.prototype.slice.call(document.getElementsByTagName('*')).reverse(), []);
 };
 
 var getElementsByClassNameHelper = function (className, list, results) {
+	if(list.length==0){
+		return results;
+	}
+	var current = list.pop();
+	if(current.className.indexOf(className)!= -1){
+		results.push(current);
+	}
+	return getElementsByClassNameHelper(className, list, results);
 };
