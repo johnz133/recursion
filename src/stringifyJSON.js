b3130da9cd32749d2f2378de9619a079209da442
John Zhang <john@zhang.io>
2015-01-28 16:50:48 -0800

    refactor and readability

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,16 +4,12 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-	console.log(JSON.stringify(obj));
-	var type = typeof obj;
-	if(obj == null || type == 'boolean' || type == 'number'){
-		return ""+obj;
-	}
-	if(typeof obj == "string"){
-		return '"'+obj+'"';
-	}
 	if(obj instanceof Object){
 		return stringifyJSONHelper(obj, []);
+	} else if(typeof obj == "string"){
+		return '"' + obj + '"';
+	} else {
+		return "" + obj;
 	}
 };
 
@@ -21,7 +17,7 @@ var stringifyJSONHelper = function(obj, results) {
 	if(obj instanceof Array){
 		for(var i = 0; i < obj.length; i++){
 			if(typeof obj[i] == 'string'){
-				results.push('"'+obj[i]+'"');
+				results.push('"' + obj[i] + '"');
 			}
 			else if(obj[i] instanceof Object){
 				results.push(stringifyJSONHelper(obj[i], []));
@@ -30,7 +26,7 @@ var stringifyJSONHelper = function(obj, results) {
 				results.push(obj[i]);
 			}
 		}
-		return "["+results +"]";
+		return "[" + results + "]";
 	} else {
 		for(var key in obj){
 			if(typeof obj[key] != 'function' && obj[key] !== undefined){
