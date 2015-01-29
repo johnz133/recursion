e79b519ceabf6081c3a5fd578127e5d1a48683e2
John Zhang <john@zhang.io>
2015-01-28 17:15:41 -0800

    fixed getbyclassname, used document.body, element.classlist and .childnodes

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,16 +5,18 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-	return getElementsByClassNameHelper(className, Array.prototype.slice.call(document.getElementsByTagName('*')).reverse(), []);
+	return getElementsByClassNameHelper(className, document.body, []);
 };
 
-var getElementsByClassNameHelper = function (className, list, results) {
-	if(list.length==0){
-		return results;
+var getElementsByClassNameHelper = function (className, node, results) {
+	if(node.classList != undefined && node.classList.contains(className)){
+		results.push(node);
 	}
-	var current = list.pop();
-	if(current.className.indexOf(className)!= -1){
-		results.push(current);
+	var children = node.childNodes;
+	if(children.length > 0){
+		for(var i = 0; i < children.length; i++){
+			results = getElementsByClassNameHelper(className, children[i], results);
+		}
 	}
-	return getElementsByClassNameHelper(className, list, results);
+	return results;
 };
